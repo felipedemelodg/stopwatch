edf774ac32e3d8e22bd06159891aed91b47aa255
Felipe de Melo <felipedemelodg@gmail.com>
2024-04-30 20:42:30 -0300

    entity

---
+++ src/core/entities/Entity.ts
@@ -0,0 +1,20 @@
+export class Entity {
+  constructor(
+    private hours: number,
+    private minutes: number,
+    private seconds: number
+  ) {}
+
+  static pad(num: number): string {
+    return num < 10 ? "0" : num.toString();
+  }
+  toString(): string {
+    return (
+      Entity.pad(this.hours) +
+      ":" +
+      Entity.pad(this.minutes) +
+      ":" +
+      Entity.pad(this.seconds)
+    );
+  }
+}
