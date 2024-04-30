7f52a89380e2dfa581186aea27271ca42062c2cc
Felipe de Melo <felipedemelodg@gmail.com>
2024-04-30 20:50:53 -0300

    service

---
+++ src/core/services/Services.ts
@@ -0,0 +1,43 @@
+import { Entity } from "../entities/Entity";
+
+export interface ServiceInterface {
+  start(): void;
+  stop(): void;
+  reset(): void;
+  getTime(): Entity;
+}
+
+export class ServiceInterface implements ServiceInterface {
+  private hours: number = 0;
+  private minutes: number = 0;
+  private seconds: number = 0;
+  private timeInterval: any;
+
+  start(): void {
+    this.timeInterval = setInterval(() => this.updateTime(), 1000);
+  }
+  stop(): void {
+    clearInterval(this.timeInterval);
+  }
+  reset(): void {
+    clearInterval(this.timeInterval);
+    this.hours = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+  }
+  getTime(): Entity {
+    return new Entity(this.hours, this.minutes, this.seconds);
+  }
+
+  private updateTime(): void {
+    this.seconds++;
+    if (this.seconds === 60) {
+      this.seconds = 0;
+      this.minutes++;
+      if (this.minutes === 60) {
+        this.minutes = 0;
+        this.hours++;
+      }
+    }
+  }
+}
